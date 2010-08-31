dab41ac8293d8bd52b594633f49dab80c6c9a6bd
Zhu Jia Cheng <realalien@gmail.com>
2010-08-31 19:54:37 +0800

    daily update

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -27,33 +27,66 @@ class StatController < ApplicationController
         # * the supposing way to retrieve verions of reponses is
         #   a sheet -> some responses(of the user by default) ; a response -> some versions
         #   it looks through questions may cause errors because of the model relations
-        logger.debug "-------------------------------------"
         logger.debug "---------///////  @survey_sheet.id: #{@survey_sheet.id}"
         logger.debug "//////// -----  @survey_sheet.responses.size : |||||    #{@survey_sheet.responses.size}"
-        logger.debug "~~~~~~~~~~~~~~~            ~~~~~~~~~~~~~~"
         
         rating_history = {}
         @survey_sheet.responses.each do | resp |
           logger.debug " @@@@@@@@@ Try to locating response,  checking response with question id #{resp.question.id}, creteria: #{params[:question_id]}"
-          if resp.question.id == params[:question_id]   #TODO: refactor
-            logger.debug "//////// find target response of question #{params[:question_id]}  |||||  #{@survey_sheet.responses.response_versions.size}"
+          
+          # due to class of different number
+          if resp.question.id.to_s == params[:question_id]   #TODO: refactor
+            logger.debug "//////// find target response of question #{params[:question_id]}  |||||  #{resp.response_versions.size}"
+            
+            # Note: due to ajax-rating, each star rating operation will create a version record 
+            #       for a response (imply: any time user click the stars, the average value will be affected!)
+            #       For the moment, instead of preventing users from inputing, here I will evaluate the revisions
+            #       based on new value and input timestamp ( which can be used to distinguish unintentional input; 
+            #       eliminate new revisions caused by clicking Save comment button;)
+            
+            rating_history = apply_data_filtering_just_for_presentation(rating_history)  # try to reuse in data processing. # ATTENTION: rule must be explicit!!!!
+            
+            # To solve the problem,  following ideas can be tried out.
+            # * limit the user to do the rating less regularly, maybe weekly, monthly or quarterly.
+            # * MORE: any steps ( collecting, counting, calculating, presenting) for this auditing/statistic function.
+            
+            previous_rating = 0 
             resp.response_versions.each do | ver |
-              logger.debug "~~~~~~~~~~~~~~~  for version #{ver.version} ==== rating : #{ver.rating} , answer_text: #{ver.updated_at} "
-              rating_history.update( { ver.updated_at.to_s => ver.rating }) 
+              logger.debug "~~~~~~~~~~~~~~~  for version #{ver.version} ==== rating : #{ver.rating} , timestamp #{ver.updated_at} "
+              rating_history.update( { ver.updated_at.to_s => ver.rating } ) 
             end
           end
         end
-        logger.debug "-------------------------------------"
-      
         chart = gen_response_version_graph rating_history, "of question # #{params[:question_id]}"
-    
         render :text => chart.to_s # even without data diagram can be displaced to be a placeholder
-      
       end
   end
 
-
+  # TODO: presents in a monthly/quarterly manner
   
+  # all_time_rating_values, 
+  def apply_data_filtering_just_for_presentation(all_time_rating_values)
+      
+      filtered = {}
+      
+      previous_timestamp = nil
+      previous_rating = -1
+      all_time_rating_values.sort.each_with_index do | map , idx |
+          
+          # scenarion and rule: if within one day and rating not change, 
+          logger.debug "map   key: #{map.key}  =>  value #{map.value} ....  idx :  #{idx}"
+          
+          # copy to timestamp for comparing and judging
+          previous_timestamp = map.key
+          previous_rating = map.value
+          
+      end
+      
+      filtered = all_time_rating_values
+      return filtered
+  end
+
+
   # rating_history: a timestamp-rating map, sorted by key
   def gen_response_version_graph(rating_history, question_used_as_title)
       
@@ -93,7 +126,7 @@ class StatController < ApplicationController
   
       x_legend = XLegend.new("2010 Monthly")
       x_legend.set_style('{font-size: 20px; color: #778877}')
-      y_legend = YLegend.new("Rating")
+      y_legend = YLegend.new("Monthly Average Rating")
       y_legend.set_style('{font-size: 20px; color: #770077}')
 
       chart =OpenFlashChart.new
